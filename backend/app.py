d5592fe86deac881694239dcee9e0b2c6b5066e1
Daniel Li <ningyl1@uci.edu>
2024-12-24 22:34:23 -0700

    scrapped defensive rating idea

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import time
-from nba_api.stats.endpoints import playergamelog, leaguedashteamstats, defensehub
+from nba_api.stats.endpoints import playergamelog, leaguedashteamstats
 from nba_api.stats.static import players, teams
 import pandas
 
@@ -18,8 +18,6 @@ def get_player_id(player_name):
 def get_team_stats(season):
     team_stats = leaguedashteamstats.LeagueDashTeamStats(season=season).get_data_frames()[0]
     time.sleep(2)
-    #defence=defensehub.DefenseHub(season=season).get_data_frames()[3]
-    time.sleep(2)
     team_stats['Possessions'] = 0.5 * (
         team_stats['FGA'] + 
         0.4 * team_stats['FTA'] - 
@@ -27,7 +25,6 @@ def get_team_stats(season):
         team_stats['TOV']
     )/team_stats['GP']
     team_stats['REB']=team_stats['REB']/team_stats['GP']
-    team_stats['defrating']=0
     return team_stats.set_index('TEAM_ID')
 
 @app.route('/player-stats', methods=['GET'])
@@ -58,7 +55,7 @@ def player_stats():
                     player_min = game['MIN']
 
                     if opponent_team_id in team_stats.index:
-                        opponent_def_rating = team_stats.at[opponent_team_id, 'defrating']
+                        opponent_def_rating = None
                         opponent_reb = team_stats.at[opponent_team_id, 'REB']
                         opponent_possessions = team_stats.at[opponent_team_id, 'Possessions']
                     else:
